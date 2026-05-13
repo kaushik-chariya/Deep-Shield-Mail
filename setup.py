b38cfae854d3010faaf32d974455899f36df408c
kaushik013 <chariyakaushik1435@gmail.com>
2026-05-13 15:41:54 +0530

    initial project scaffold

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Deep-Shield-Mail",
+    version="0.0.1",
+    author="Kaushik-chariya",
+    author_email="chariyakaushik1435@gmail.com",
+    packages=find_packages()
+)
